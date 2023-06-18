de18f3e45e2e79c0293b1c7cc2870fa3c1ebfa49
Dmitry Bondarchuk <dimitrywebdeveloper@gmail.com>
2023-06-18 20:44:56 +0300

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 use std::fs;
 
-const START_FIRST_TAG_SYMBOL: &str = "<";
-const START_LAST_TAG_SYMBOL: &str = "</";
-const END_TAG_SYMBOL: &str = ">";
+const START_TAG_OPEN_SYMBOL: &str = "<";
+const END_TAG_OPEN_SYMBOL: &str = "</";
+const END_TAG_CLOSE_SYMBOL: &str = ">";
 
 struct WebsiteInfoForScraping {
     part_of_attribute_to_search: String,
-    scraping_tag_start: String,
-    scraping_tag_end: String,
+    search_tag: String,
     url: String,
     website_name: String
 }
@@ -17,15 +16,13 @@ async fn main() {
     let website_list: Vec<WebsiteInfoForScraping> = vec![
         WebsiteInfoForScraping {
             part_of_attribute_to_search: "".to_string(),
-            scraping_tag_start: "<p".to_string(),
-            scraping_tag_end: "</p".to_string(),
+            search_tag: "p".to_string(),
             url: "https://doc.rust-lang.org/book/".to_string(),
             website_name: "rust_book".to_string()
         },
         WebsiteInfoForScraping {
             part_of_attribute_to_search: "class=\"toggle\"".to_string(),
-            scraping_tag_start: "<summary".to_string(),
-            scraping_tag_end: "</summary".to_string(),
+            search_tag: "summary".to_string(),
             url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript".to_string(),
             website_name: "wdeveloper.mozilla.org".to_string()
         },
@@ -41,17 +38,15 @@ async fn main() {
 
         let part_of_attribute = website.part_of_attribute_to_search;
 
-        let summary_tag_start = website.scraping_tag_start;
-        let summary_tag_end = website.scraping_tag_end;
+        let summary_tag_start = format!("{}{}", START_TAG_OPEN_SYMBOL, &website.search_tag);
+        let summary_tag_end = format!("{}{}", END_TAG_OPEN_SYMBOL, &website.search_tag);
 
-        let mut result: Vec<String> = Vec::new();
-
-        let mut search_process = true;
+        let mut search_in_progress = true;
 
         scraping_result.push(format!("---------------start scraping from the {}---------------", website.website_name));
         scraping_result.push(format!("website url: {}", website.url));
 
-        while search_process {
+        while search_in_progress {
 
             let find_attribute = response.find(&part_of_attribute);
 
@@ -64,29 +59,25 @@ async fn main() {
                 if find_summary_tag_start.is_some() && find_summary_tag_end.is_some() {
                     response = response[find_summary_tag_start.unwrap()..response.len()].to_string();
 
-                    let find_end_first_tag_symbol = response.find(END_TAG_SYMBOL);
-                    let find_start_last_tag_symbol = response.find(START_LAST_TAG_SYMBOL);
+                    let find_end_first_tag_symbol = response.find(END_TAG_CLOSE_SYMBOL);
+                    let find_start_last_tag_symbol = response.find(END_TAG_OPEN_SYMBOL);
 
                     if find_end_first_tag_symbol.is_some() && find_start_last_tag_symbol.is_some() {
-                        result.push(response[(find_end_first_tag_symbol.unwrap() + 1)..find_start_last_tag_symbol.unwrap()].to_string());
+                        scraping_result.push(response[(find_end_first_tag_symbol.unwrap() + 1)..find_start_last_tag_symbol.unwrap()].to_string());
                     }
                     response = response[find_start_last_tag_symbol.unwrap()..response.len()].to_string();
                 } else {
-                    search_process = false
+                    search_in_progress = false
                 }
             } else {
-                search_process = false
+                search_in_progress = false
             }
         }
 
-        println!("{:#?}", &result);
-
-        result.into_iter().for_each(|element| {
-            scraping_result.push(element);
-        });
-
         scraping_result.push(format!("---------------end of scraping from {}---------------", website.website_name));
     }
+
+    println!("{:#?}", &scraping_result);
 
     let file_result = "result.txt";
 
